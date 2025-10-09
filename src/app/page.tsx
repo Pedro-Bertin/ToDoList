b0dd43a51d40b6e3677e874f8b28582b379a8bc8
Pedro-bertin <pedro-bertin@live.com>
2025-10-09 02:09:57 -0300

    Segundo commit fazendo o projeto.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,57 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Plus, List, Check, MoveDownRight } from "lucide-react";
+
 const Home = () => {
-  return <h1>To Do List</h1>;
-}
+  return (
+    <main className="w-full h-screen flex justify-center items-center bg-gray-100 p-10">
+      <Card className="w-lg">
+        <CardHeader className="flex gap-2">
+          <Input placeholder="Adicionar tarefa" />
+          <Button variant="default" className="cursor-pointer">
+            <Plus />
+            Adicionar
+          </Button>
+        </CardHeader>
+
+        <CardContent>
+          <Separator className="mb-4" />
+          <div className="flex gap-2">
+            <Badge className="cursor-pointer">
+              <List />
+              Tarefas
+            </Badge>
+            <Badge className="cursor-pointer">
+              <Check />
+              check
+            </Badge>
+            <Badge className="cursor-pointer">
+              <MoveDownRight />
+              Tarefas
+            </Badge>
+          </div>
+
+          <div className="h-14">
+            <div className="bg-green-600 h-8">
+              <div className="w-2 h-full "></div>
+              <p className="text-center text-gray-500 mt-4">Estudar React</p>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </main>
+  );
+};
 
 export default Home;
